346c8910f647edb73f55febcf73b9b5e3d3c01ca
sovello <sovellohpmgani@gmail.com>
2015-05-18 19:57:51 -0400

    generates some shapes most of them circular and rectangular. more functions to be added to accommodate for more varied patterns

--- random_art.py
+++ random_art.py
@@ -1,60 +1,28 @@
 import random
-from math import pi, sin, cos
-
-# Your job is to create better version of create_expression and
-# run_expression to create random art.
-# Your expression should have a __str__() function defined for it.
-
-class Expression:
-    def __init__(self):
-        self.commands = []
-
-    def evaluate(self, x, y):
-        value = 1
-        for (command, coord) in self.commands:
-            if command == "sin" and coord == "x":
-                value *= sin(pi * x)
-            elif command == "sin" and coord == "y":
-                value *= sin(pi * y)
-            elif command == "cos" and coord == "x":
-                value *= cos(pi * x)
-            elif command == "cos" and coord == "y":
-                value *= cos(pi * y)
-
-        return value
-
-    def __str__(self):
-        return str(self.commands)
-
-
-def avg(a, b):
-    return (a + b) / 2
+from math import pi, sin, cos, tan, exp
 
 
 def create_expression():
     """This function takes no arguments and returns an expression that
     generates a number between -1.0 and 1.0, given x and y coordinates."""
-    # expr = lambda x, y: (random.random() * 2) - 1
-    # return expr
+    expressions = [
+        'pi*(x**2 + y**2)', # circle
+        'sin(x)', # gradient horizontal
+        'cos(x)', # gradient horizontal center-hollow
+        'cos(x)+sin(x)',
+        'abs(x)',
+        '2*x**2*y**2', # corner circles
+        'x**5*y**2+3*x*3+2*x**2+x**4', # thick gradient
+        'exp(pi*sin(x))', # lighter gradient than sin(x)
+        'sin(pi*x)*cos(x)*pi*(y**2 + y**2)', # reflecting half circles
+        'sin(2*x**2*y**2)',
+    ]
 
-    expr = Expression()
-    for _ in range(3):
-        if random.random() > 0.5:
-            x_or_y = "x"
-        else:
-            x_or_y = "y"
-
-        if random.random() > 0.5:
-            sin_or_cos = "sin"
-        else:
-            sin_or_cos = "cos"
-
-        expr.commands.append([sin_or_cos, x_or_y])
-
-    return expr
+    return random.choice(expressions)
+    
 
 def run_expression(expr, x, y):
     """This function takes an expression created by create_expression and
     an x and y value. It runs the expression, passing the x and y values
     to it and returns a value between -1.0 and 1.0."""
-    return expr.evaluate(x, y)
+    return eval(expr)
